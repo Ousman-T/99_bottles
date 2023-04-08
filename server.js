e32bbc00873fad5c3466f4f14667244247b9dd50
Ousman Touray <otouray14@gmail.com>
2023-04-08 14:44:30 -0400

    complete

--- server.js
+++ server.js
@@ -10,11 +10,19 @@ app.get('/', (req,res) => {
 });
 
 app.get('/98', (req, res) => {
-    res.send('<h1>take one down, pass it around</h1>');
+    res.send('<h1>98 Bottles of Beer on the wall!</h1> <a href="/"> take one down, pass it around.</h1>');
 });
 
 app.get('/:number_of_bottles', (req, res) => {
-    res.send('Hello world!');
+    // very tricky if else statement. Never did ${whatever -1}
+    if(req.params.number_of_bottles > 0){
+        res.send(
+            `<h1>${req.params.number_of_bottles} Bottles of Beer on the wall </h1>
+            <a href="/${req.params.number_of_bottles -1}"> Take one down, pass it around</a>`
+        )
+    } else {
+        res.send(`<h1>${req.params.number_of_bottles} No more bottles of Beer on the Wall!</h1> <a href='/'> Start Over?</a>`)
+    }
 })
 
 
